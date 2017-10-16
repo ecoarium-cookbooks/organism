22ababb8d5931bb02daac9122d0242710b33cec1
build <build@fake.com>
2017-10-16 09:46:15 -0400

    initial import

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'organism'
+maintainer       'Jay Flowers'
+maintainer_email 'jay.flowers@gmail.com'
+license          'Apache License 2.0'
+description      'Installs/Configures organism'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,4 @@
+#
+# Cookbook Name:: organism
+# Recipe:: default
+#
